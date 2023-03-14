8a826d6143c80f2dcba73488d8742f5039075bc1
fiodar.rekish <fiodar.rekish@exactprosystems.com>
2023-03-14 19:08:53 +0400

    comment clean-up

--- main.go
+++ main.go
@@ -20,27 +20,14 @@ func main() {
 
 	messagesChannel := make(chan controller.RawMessageFromClient, 1000)
 	clientDisconnectChannel := make(chan controller.ClientId, 1000)
-	// stopListenningChannel := make(chan bool, 1)
 	go logger.StartLogger(messagesChannel, clientDisconnectChannel, &mainWaitGroup)
 
 	listeningServer := controller.New(HOST+":"+PORT, messagesChannel, clientDisconnectChannel, &mainWaitGroup)
 	go serverControl(messagesChannel, &mainWaitGroup, &listeningServer)
 	listeningServer.Start()
 
-	// go controller.StartListening(HOST+":"+PORT, messagesChannel, clientDisconnectChannel, stopListenningChannel)
 	mainWaitGroup.Wait()
 
-	// loggingChannel <- ClientTwo()
-	// time.Sleep(time.Second * 1)
-	// loggingChannel <- ClientOne()
-	// time.Sleep(time.Second * 1)
-	// loggingChannel <- ClientTwo()
-	// loggingChannel <- ClientOne()
-
-	// time.Sleep(time.Second * 1)
-	// close(loggingChannel)
-
-	// fmt.Printf("[11][1][client one]: %v\n", model.ParseRawMessage("[11][1][client one]"))
 }
 
 func serverControl(messagesChannel chan controller.RawMessageFromClient, mainWaitGroup *sync.WaitGroup, listeningServer *controller.ListenerServer) {
@@ -58,26 +45,3 @@ func serverControl(messagesChannel chan controller.RawMessageFromClient, mainWai
 		}
 	}
 }
-
-// func ClientOne() controller.RawMessageFromClient {
-// 	return controller.RawMessageFromClient{
-// 		RawMessage: model.RawMessage{
-// 			ParsedMessage: model.ParsedMessage{Payload: "client one", Seq_num: 1},
-// 			Length:        11,
-// 		},
-// 		ClientId: controller.ClientId{
-// 			Address: "8081",
-// 		},
-// 	}
-// }
-
-// func ClientTwo() controller.RawMessageFromClient {
-// 	return controller.RawMessageFromClient{
-// 		RawMessage: model.RawMessage{
-// 			ParsedMessage: model.ParsedMessage{Payload: "client two", Seq_num: 2},
-// 			Length:        22,
-// 		}, ClientId: controller.ClientId{
-// 			Address: "8082",
-// 		},
-// 	}
-// }
