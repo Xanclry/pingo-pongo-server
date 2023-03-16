54a9559a229021fbc1e775173a17f513b26c6209
fiodar.rekish <fiodar.rekish@exactprosystems.com>
2023-03-16 19:35:49 +0400

    removed unused methods

--- model/message.go
+++ model/message.go
@@ -2,8 +2,6 @@ package model
 
 import (
 	"fmt"
-	"strconv"
-	"strings"
 )
 
 type ParsedMessage struct {
@@ -13,12 +11,6 @@ type ParsedMessage struct {
 
 func (parsed ParsedMessage) String() string {
 	return fmt.Sprintf("[%v][%v]", parsed.Seq_num, parsed.Payload)
-}
-
-func (parsed ParsedMessage) ConvertToRawMessage() RawMessage {
-	length := calculateLengthOfStringWithLength(parsed.String())
-	return RawMessage{ParsedMessage: parsed, Length: length}
-
 }
 
 // [msg length uint16][sequence number uint32][payload any string]
@@ -35,20 +27,6 @@ func (raw RawMessage) String() string {
 	return fmt.Sprintf("[%v]%v", raw.Length, raw.GetParsedMessage().String())
 }
 
-func ParseRawMessage(s string) RawMessage {
-	splitted := strings.Split(s, "]")
-	length, _ := strconv.ParseUint(splitted[0][1:], 10, 16)
-	seq_num, _ := strconv.ParseUint(splitted[1][1:], 10, 32)
-	payload := splitted[2][1:]
-	return RawMessage{
-		ParsedMessage: ParsedMessage{
-			Seq_num: uint32(seq_num),
-			Payload: payload,
-		},
-		Length: uint16(length),
-	}
-}
-
 // [msg length uint16][sequence number uint32]
 type Response struct {
 	Length  uint16
@@ -56,29 +34,5 @@ type Response struct {
 }
 
 func (response Response) String() string {
-	return BuildStringWithLength("[" + strconv.FormatUint(uint64(response.Seq_num), 10) + "]")
-}
-
-func calculateLengthOfStringWithLength(s string) uint16 {
-	stringLength := len(s) + 2
-	stringLengthAsString := strconv.Itoa(stringLength)
-	totalLength := stringLength + len(stringLengthAsString)
-
-	actualLength := len(fmt.Sprintf("[%v]%v", totalLength, s))
-	if actualLength != totalLength {
-		totalLength = actualLength
-	}
-	return uint16(totalLength)
-}
-
-func BuildStringWithLength(s string) string {
-	stringLength := len(s) + 2
-	stringLengthAsString := strconv.Itoa(stringLength)
-	totalLength := stringLength + len(stringLengthAsString)
-
-	actualLength := len(fmt.Sprintf("[%v]%v", totalLength, s))
-	if actualLength != totalLength {
-		totalLength = actualLength
-	}
-	return fmt.Sprintf("[%v]%v", totalLength, s)
+	return fmt.Sprintf("[%v][%v]", response.Length, response.Seq_num)
 }
